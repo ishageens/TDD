33ab84e7b34814c5855b6dd672955dd9a3b475da
Isha <ishageens@msn.com>
2021-02-04 09:38:51 +0100

    Assert methods

--- TDDLibraryTest/JaarTest.cs
+++ TDDLibraryTest/JaarTest.cs
@@ -9,25 +9,29 @@ namespace TDDLibraryTest
         [TestMethod]
         public void EenJaarDeelbaarDoor400IsEenSchrikkeljaar()
         {
-            Assert.AreEqual(true, new Jaar(2000).IsSchrikkeljaar);
+            //Assert.AreEqual(true, new Jaar(2000).IsSchrikkeljaar);
+            Assert.IsTrue(new Jaar(2000).IsSchrikkeljaar);
         }
 
         [TestMethod]
         public void EenJaarDeelbaarDoor100MaarNietDoor400IsGeenSchrikkeljaar()
         {
-            Assert.AreEqual(false, new Jaar(1900).IsSchrikkeljaar);
+            //Assert.AreEqual(false, new Jaar(1900).IsSchrikkeljaar);
+            Assert.IsFalse(new Jaar(1900).IsSchrikkeljaar);
         }
 
         [TestMethod]
         public void EenJaarDeelbaarDoor4IsEenSchrikkeljaar()
         {
-            Assert.AreEqual(true, new Jaar(2012).IsSchrikkeljaar);
+            //Assert.AreEqual(true, new Jaar(2012).IsSchrikkeljaar);
+            Assert.IsTrue(new Jaar(2012).IsSchrikkeljaar);
         }
 
         [TestMethod]
         public void EenJaarNietDeelbaarDoor4IsGeenSchrikkeljaar()
         {
-            Assert.AreEqual(false, new Jaar(2015).IsSchrikkeljaar);
+            //Assert.AreEqual(false, new Jaar(2015).IsSchrikkeljaar);
+            Assert.IsFalse(new Jaar(2015).IsSchrikkeljaar);
         }
     }
 }
