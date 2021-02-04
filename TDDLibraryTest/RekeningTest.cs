763bc8ecef50a1ee576a5547475dd484ea055dd1
Isha <ishageens@msn.com>
2021-02-04 11:26:51 +0100

    Hoofdstuk 4 Test Fixtures

--- TDDLibraryTest/RekeningTest.cs
+++ TDDLibraryTest/RekeningTest.cs
@@ -6,17 +6,25 @@ namespace TDDLibraryTest
     [TestClass]
     public class RekeningTest
     {
+        private Rekening rekening;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            rekening = new Rekening();
+        }
+
         [TestMethod]
         public void SaldoNieuweRekeningIsNul()
         {
-            var rekening = new Rekening();
+            //var rekening = new Rekening();
             Assert.AreEqual(decimal.Zero, rekening.Saldo);
         }
 
         [TestMethod]
         public void SaldoNaEersteStortingIsGelijkAanGestortBedrag()
         {
-            var rekening = new Rekening();
+            //var rekening = new Rekening();
             var bedrag = 2.5m;
             rekening.Storten(bedrag);
             Assert.AreEqual(rekening.Saldo, bedrag);
@@ -25,7 +33,7 @@ namespace TDDLibraryTest
         [TestMethod]
         public void SaldoNaTweeStortingenIsGelijkAanSomVanBeideBedragen()
         {
-            var rekening = new Rekening();
+            //var rekening = new Rekening();
             var bedrag1 = 2.5m;
             rekening.Storten(bedrag1);
             var bedrag2 = 1m;
