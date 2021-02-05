c44375412e4a58550f063cee24713fc454149fc1
Isha <ishageens@msn.com>
2021-02-05 10:32:09 +0100

    Hoofdstuk 5 Verzameling Testen

---
+++ TDDCursusLibrary/Statistiek.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TDDCursusLibrary
+{
+    public class Statistiek
+    {
+
+        public static decimal Gemiddelde(decimal[] getallen)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
