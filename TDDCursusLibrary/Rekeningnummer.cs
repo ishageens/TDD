09ca71a6036d3f5f23e88d5f893ec7fe9b3fe452
Isha <ishageens@msn.com>
2021-02-05 10:12:17 +0100

    Hoofdstuk 5: Waarden testen

---
+++ TDDCursusLibrary/Rekeningnummer.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TDDCursusLibrary
+{
+    public class Rekeningnummer
+    {
+        public Rekeningnummer(string nummer)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string ToString()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
