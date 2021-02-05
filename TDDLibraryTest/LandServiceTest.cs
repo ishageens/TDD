db46afe3081ef5efcd28879cf73e56abff20f38b
Isha <ishageens@msn.com>
2021-02-05 13:21:37 +0100

    Hoofdstuk 8 Mock

--- TDDLibraryTest/LandServiceTest.cs
+++ TDDLibraryTest/LandServiceTest.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using TDDCursusLibrary;
+using System;
+using Moq;
 
 namespace TDDCursusLibraryTest
 {
@@ -8,11 +10,20 @@ namespace TDDCursusLibraryTest
     {
         private ILandDAO landDAO; // Stub 
         private LandService landService; // Te testen class 
+        private Mock<ILandDAO> mockFactory;
 
         [TestInitialize]
         public void Initialize()
         {
-            landDAO = new LandDAOStub();
+            // landDAO = new LandDAOStub();
+            mockFactory = new Mock<ILandDAO>();
+            landDAO = mockFactory.Object;
+
+            mockFactory.Setup(eenLandDAO => eenLandDAO.OppervlakteAlleLanden()).Returns(20);
+            mockFactory.Setup(eenLandDAO => eenLandDAO.Read("B")).Returns(new Land { Landcode = "B", Oppervlakte = 5 });
+            mockFactory.Setup(eenLandDAO => eenLandDAO.Read("NL")).Returns(new Land { Landcode = "NL", Oppervlakte = 6 });
+            mockFactory.Setup(eenLandDAO => eenLandDAO.Read(string.Empty)).Throws(new ArgumentException());
+
             landService = new LandService(landDAO);
         }
 
@@ -20,6 +31,10 @@ namespace TDDCursusLibraryTest
         public void FindVerhoudingOppervlakteLandTovOppervlakteAlleLanden()
         {
             Assert.AreEqual(0.25m, landService.FindVerhoudingOppervlakteLandTovOppervlakteAlleLanden("B"));
+            // hier gaan we straks verifiëren of landService de methods 
+            // read("B") en OppervlakteAlleALanden() heeft opgeroepen op landDAO. 
+            mockFactory.Verify(eenLandDAO => eenLandDAO.OppervlakteAlleLanden());
+            mockFactory.Verify(eenLandDAO => eenLandDAO.Read("B"));
         }
     }
 }
